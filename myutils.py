f8e0ae15ca3535448915f2ce190c484b3a5d262c
asun975 <65915716+asun975@users.noreply.github.com>
2024-06-19 22:59:17 -0400

    Update myutils.py
    change results to dict,
    modified function plot loss curves from zero to mastery pytorch course by mdbourke (github)

--- myutils.py
+++ myutils.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-
+# function from zero to mastery pytorch course by mdbourke (github).
 def print_train_time(start, end, device=None):
     """Prints difference between start and end time.
 
@@ -15,9 +15,42 @@ def print_train_time(start, end, device=None):
     total_time = end - start
     print(f"\nTrain time on {device}: {total_time:.3f} seconds")
     return total_time
-
+    
+# modified from zero to mastery pytorch course by mdbourke (github).
 # Plot loss curves of a model
 def plot_loss_curves(results):
+    """Plots training curves of a results dictionary.
+
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "test_acc": [...]}
+    """
+    loss = results["train_loss"]
+    test_accuracy = results["test_acc"]
+
+    batches = range(len(results["train_loss"]))
+    epochs = range(len(results["test_acc"]))
+
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(batches, loss, label="train_loss")
+    plt.title("Loss")
+    plt.xlabel("Mini Batches of 10")
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+# modified from zero to mastery pytorch course by mdbourke (github).
+# Plot loss curves of a model
+def plot_loss_curves_old(results):
     """Plots training curves of a results model checkpoints.
 
     Args:
@@ -26,7 +59,7 @@ def plot_loss_curves(results):
              "test_acc": [...],}
         for each saved model checkpoint.
     """
-
+    
     # Plot training loss
     plt.figure(figsize=(15, 7))
     plt.subplot(1, 2, 1)
